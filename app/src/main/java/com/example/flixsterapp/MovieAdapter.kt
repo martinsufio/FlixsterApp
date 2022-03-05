7e3601878d9869ea10470de8a06497196145fd3c
stillflow <72228870+stillflow@users.noreply.github.com>
2022-03-04 22:11:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixsterapp/MovieAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.flixsterapp
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+
+class MovieAdapter(private val context: Context, private val movies: List<Movie>)
+    : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       val View = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(View)
+    }
+
+    override fun onBindViewHolder(holder: MovieAdapter.ViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+
+    }
+
+    override fun getItemCount() = movies.size
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener{
+        private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
+        private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+        private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init{
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(movie: Movie){
+            tvTitle.text = movie.title
+            tvOverview.text = movie.overview
+            Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
+
+        }
+    }
+}
